149e76119f339c1b2d6148b0416ffc061b979194
RomanGandaloev <romangandaloev@gmail.com>
2025-01-09 20:45:01 +0700

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,14 @@
+# 1 задание
+user_name = "Виктор"
+title = "Проект ДВК"
+content = "Выполнить расчеты по предложениям ДВК"
+status = "В работе"
+created_date = "09-01-25"
+issue_date = "09-02-25"
+
+print("Имя работника:", user_name)
+print("Название работы:", title)
+print("Задача:", content)
+print("Статус выполнения:", status)
+print("Дата начала работы", created_date)
+print("Крайняя дата сдачи", issue_date)
