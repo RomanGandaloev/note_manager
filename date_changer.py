448661c47cc6398f05f77214fc6ac1f50e2f0f77
RomanGandaloev <romangandaloev@gmail.com>
2025-01-10 21:12:23 +0700

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,13 @@
+user_name = "Виктор"
+title = "Проект ДВК"
+content = "Выполнить расчеты по предложениям ДВК"
+status = "В работе"
+created_date = "09-01-25"
+issue_date = "09-02-25"
+
+print("Имя работника:", user_name)
+print("Название работы:", title)
+print("Задача:", content)
+print("Статус выполнения:", status)
+print("Дата начала работы", created_date[:-3:])
+print("Крайняя дата сдачи", issue_date[:-3:])
