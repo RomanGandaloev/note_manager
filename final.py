9519890e3f5f26af998ddd56e1ffe0f95f3164af
RomanGandaloev <romangandaloev@gmail.com>
2025-01-15 20:00:23 +0700

    Add files via upload

---
+++ final.py
@@ -0,0 +1,17 @@
+note = [
+    'user_name',
+    ['title0', 'title1', 'title2',],
+    'content',
+    'status',
+    'created_date',
+    'issue_date'
+]
+user_name = input('Введите свое имя: ')
+title0 = input('Введите первый заголовок заметки: ')
+title1 = input('Введите второй заголовок заметки: ')
+title2 = input('Введите третий заголовок заметки: ')
+content = input('Введите список выполняемых задач: ')
+status = input('Подвердите статус выполнения: ')
+created_date = input('Введите дату начала работы (в формате ДД.ММ.ГГГГ): ')
+issue_date = input('Введите дату выполнения работы (в формате ДД.ММ.ГГГГ): ')
+print(note)
